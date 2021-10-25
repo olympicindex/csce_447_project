c97bc3526744cd68b999d0b81596b49f77ec631f
Yingtao Jiang <entao@Yingtaos-MacBook-Air.local>
2021-10-25 17:20:37 -0500

    several great changes

---
+++ bubble_chart.js
@@ -0,0 +1,77 @@
+// // Step 1
+// var data = [
+//   {source:"Item 1", x: 100, y: 60, val: 1350, color: "#C9D6DF"},
+//   {source:"Item 2", x: 30, y: 80, val: 2500, color: "#F7EECF"},
+//   {source:"Item 3", x: 50, y: 40, val: 5700, color: "#E3E1B2"},
+//   {source:"Item 4", x: 190, y: 100, val: 30000, color: "#F9CAC8"},
+//   {source:"Item 5", x: 80, y: 170, val: 47500, color: "#D1C2E0"}
+// ]
+
+
+// // Step 3
+// var svg = d3.select("svg")
+//           .attr("width", 500)
+//           .attr("height", 500);
+
+// // Step 4
+// svg.selectAll("circle")
+//   .data(data).enter()
+//   .append("circle")
+//   .attr("cx", function(d) {return d.x})
+//   .attr("cy", function(d) {return d.y})
+//   .attr("r", function(d) {
+//     return Math.sqrt(d.val)/Math.PI 
+//   })
+//   .attr("fill", function(d) {
+//     return d.color;
+//   });
+
+// // Step 5
+// svg.selectAll("text")
+//   .data(data).enter()
+//   .append("text")
+//   .attr("x", function(d) {return d.x+(Math.sqrt(d.val)/Math.PI)})
+//   .attr("y", function(d) {return d.y+4})
+//   .text(function(d) {return d.source})
+//   .style("font-family", "arial")
+//   .style("font-size", "12px")
+
+d3.csv("data/countries.csv", function(data) {
+    d3.csv("data/CleanedData.csv", function(medals){
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].longitude);
+            console.log(data[i].latitude);
+        }
+        // Step 3
+        var svg = d3.select("svg")
+                    .attr("width", 10000)
+                    .attr("height", 3000);
+    
+        // Step 4
+        svg.selectAll("circle")
+            .data(data).enter()
+            .append("circle")
+            .on("click", function(d) {
+        alert("on click get data" + d.id);})
+            .attr("cx", function(d) {return (d.longitude)*5+1000})
+            .attr("cy", function(d) {return (-d.latitude+90)*5})
+            .attr("r", function(d) {
+            return Math.sqrt([2500, 4000, 8000][Math.floor(Math.random() * 3)])/Math.PI 
+            })
+            // .attr("r", function(d) {
+            //     return data.filter(function(d){ return d.Year == "2008"}).
+            //     })
+            .attr("fill", function(d) {
+            return ['#C9D6DF', '#F7EECF', '#E3E1B2'][Math.floor(Math.random() * 3)];
+            });
+        
+        svg.selectAll("text")
+        .data(data).enter()
+        .append("text")
+        .attr("x", function(d) {return (d.longitude)*5+1000})
+        .attr("y", function(d) {return (-d.latitude+90)*5+4})
+        .text(function(d) {return d.ISO})
+        .style("font-family", "arial")
+        .style("font-size", "12px")
+    });
+});
