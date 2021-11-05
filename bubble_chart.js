4754e705421ef79354c77369ef164e17d07d2731
rturovilla <rturovilla@tamu.edu>
2021-11-05 01:29:14 -0500

    updates

--- bubble_chart.js
+++ bubble_chart.js
@@ -44,7 +44,7 @@ d3.csv("data/countries.csv", function(data) {
         }
         // Step 3
         var svg = d3.select("svg")
-                    .attr("width", 5000)
+                    .attr("width", 3000)
                     .attr("height", 800);
     
         // Step 4
@@ -75,3 +75,166 @@ d3.csv("data/countries.csv", function(data) {
         .style("font-size", "12px")
     });
 });
+
+
+
+
+// ----------------------------------------------
+
+
+// Editable
+var numItems = 30,
+    tlW = window.innerWidth*.95,
+    padding = 2,
+    itemH = 90,
+    openW = 400,
+    openH = window.innerHeight*.9;
+    
+// Don't change these!
+var initW = Math.floor( (tlW-padding*(numItems-1))/numItems ),
+    smallW,
+    old = "",
+    current = "";
+
+function init() { 
+
+  // Create timeline items
+  for (var i=0; i<numItems; i++) {
+    var _i;
+    if (i==0) _i = document.getElementById('i0');
+    else {
+      _i = document.getElementById('i0').cloneNode(true);
+      _i.id = "i" + i;
+      document.getElementById('timeline').appendChild(_i);
+    }
+    TweenMax.set(_i, {
+      autoAlpha:0,
+      transformOrigin:'0% 100%',
+      scaleY:.2,
+      width:initW,
+      height:itemH,      
+      x:initW*i + padding*i
+    });
+    TweenMax.to(_i, 1, {
+      delay:i/250,
+      autoAlpha:.36,
+      scaleY:1,
+      ease:Power4.easeOut
+    });
+    TweenMax.to(_i, 1.5, {
+      delay:i/250,
+      scaleY:1,
+      ease:Elastic.easeOut.config(1.5)    
+    });
+    addListeners(_i);
+  }
+  
+  document.addEventListener("keydown", keyDown);
+  window.addEventListener("resize", resize);
+  resize();
+}
+
+function addListeners(_i){
+  _i.addEventListener("mouseover", over);
+  _i.addEventListener("mouseout", out);
+  _i.addEventListener("click", click);
+}
+
+function removeListeners(_i){
+  _i.removeEventListener("mouseover", over);
+  _i.removeEventListener("mouseout", out);
+  _i.removeEventListener("click", click);
+}
+
+function over(e){
+  document.getElementById('timeline').appendChild(e.currentTarget);
+  TweenMax.to(e.currentTarget, .3, {alpha:1, scaleY:1.15, ease:Back.easeOut.config(6)});
+}
+
+function out(e){
+  TweenMax.to(e.currentTarget, .25, {alpha:.36});
+  TweenMax.to(e.currentTarget, .5, {scaleY:1, ease:Elastic.easeOut.config(.75)});
+}
+
+function keyDown(e){
+  var _n = 1;
+  if (e.keyCode==37) { // Left arrow
+    if (current!="") {
+      _n = current.id.substr(1);
+      if (_n==0) _n = numItems;
+    }
+    _n -= 1;
+  }
+  else if (e.keyCode==39) { // Right arrow
+    if (current=="") _n = numItems-2; 
+    else {
+      _n = Number(current.id.substr(1));
+      if (_n==numItems-1) _n = -1;
+    }
+    _n += 1; 
+  }
+  
+  document.getElementById('i'+_n).dispatchEvent(new MouseEvent('mouseover'));      
+  document.getElementById('i'+_n).dispatchEvent(new MouseEvent('click'));
+}
+
+function click(e){
+  if (current!="") {
+    old = current;
+    TweenMax.to(old, .5, {
+      y:0,
+      width:smallW,
+      height:itemH,
+      alpha:.36,
+      ease:Power3.easeInOut,
+      onComplete:addListeners,
+      onCompleteParams:[old]
+    });
+  }
+  current = e.currentTarget;
+  removeListeners(current);
+  TweenMax.to(current, .5, {
+    scaleY:1,
+    width:openW,
+    height:openH,
+    y:-Math.abs(itemH-openH),
+    ease:Power3.easeInOut,
+    onUpdate:update
+  });
+}
+
+function update(){
+  
+  smallW = ( (tlW-parseInt(current.style.width, 10))-(padding*(numItems)) ) / (numItems-1);
+  var _i, _pad=0, _prevW=0, _prevX=0;
+  
+  for (var i=0; i<numItems; i++) {
+    
+    _i = document.getElementById('i'+i);
+    
+    if (i>0) {
+      _prevW = parseInt(document.getElementById('i'+(i-1)).style.width, 10);
+      _prevX = parseInt(document.getElementById('i'+(i-1))._gsTransform.x, 10);
+      _pad = padding;
+    }
+    
+    // First expansion
+    if (old=="") { 
+      if ( i!=current.id.substr(1) ) TweenMax.set(_i, { width:smallW });
+    }
+    // Subsequent expansions
+    else { 
+      smallW = ( (tlW-parseInt(current.style.width, 10)-parseInt(old.style.width, 10))-(padding*(numItems)) ) / (numItems-2);
+      if ( i!=current.id.substr(1) && i!=old.id.substr(1) ) TweenMax.set(_i, { width:smallW })
+    }
+    
+    TweenMax.set(_i, { x:_prevW+_prevX+_pad })
+
+  }
+}
+
+function resize(e){
+   TweenMax.set('#timeline', {width:tlW, left:padding/2 + window.innerWidth/2 - tlW/2})
+}
+
+init();
