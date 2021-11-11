4e27d531d7da18b5ca06fe7b63a432c59b8ae080
rturovilla <rturovilla@tamu.edu>
2021-11-10 21:30:24 -0600

    added timeline

--- dataviz/csce-447/src/Navbar.js
+++ dataviz/csce-447/src/Navbar.js
@@ -1,10 +1,33 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import "./Navbar.css";
 
 
 
 function Navbar(){
+    const [value,setValue] = React.useState(0);
+    
+    const handlechange = (event,newValue)=>{
+        setValue(newValue)
+    }
     return(
-        <h1>navbar</h1>
+        <div className="timeline">
+            <BottomNavigation 
+            className = "timeline__sections" 
+            showLabels
+            value={value}
+            onChange = {(event,newValue)=>handlechange(event,newValue)}
+            >
+                <BottomNavigationAction label="2000"/>
+                <BottomNavigationAction label="2004"/>
+                <BottomNavigationAction label="2008"/>
+                <BottomNavigationAction label="2012"/>
+                <BottomNavigationAction label="2016"/>
+                <BottomNavigationAction label="2020"/>
+            </BottomNavigation>
+        </div>
     )
 }
 
