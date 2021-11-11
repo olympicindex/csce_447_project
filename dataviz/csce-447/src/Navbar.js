2adb7873674893aa91c5bf76588d1ced6ac1be69
rturovilla <rturovilla@tamu.edu>
2021-11-10 21:54:47 -0600

    updated the timeline to add all the years

--- dataviz/csce-447/src/Navbar.js
+++ dataviz/csce-447/src/Navbar.js
@@ -20,6 +20,29 @@ function Navbar(){
             value={value}
             onChange = {(event,newValue)=>handlechange(event,newValue)}
             >
+                <BottomNavigationAction label="1896"/>
+                <BottomNavigationAction label="1900"/>
+                <BottomNavigationAction label="1904"/>
+                <BottomNavigationAction label="1908"/>
+                <BottomNavigationAction label="1912"/>
+                <BottomNavigationAction label="1920"/>
+                <BottomNavigationAction label="1924"/>
+                <BottomNavigationAction label="1928"/>
+                <BottomNavigationAction label="1932"/>
+                <BottomNavigationAction label="1936"/>
+                <BottomNavigationAction label="1948"/>
+                <BottomNavigationAction label="1952"/>
+                <BottomNavigationAction label="1956"/>
+                <BottomNavigationAction label="1960"/>
+                <BottomNavigationAction label="1964"/>
+                <BottomNavigationAction label="1968"/>
+                <BottomNavigationAction label="1972"/>
+                <BottomNavigationAction label="1976"/>
+                <BottomNavigationAction label="1980"/>
+                <BottomNavigationAction label="1984"/>
+                <BottomNavigationAction label="1988"/>
+                <BottomNavigationAction label="1992"/>
+                <BottomNavigationAction label="1996"/>
                 <BottomNavigationAction label="2000"/>
                 <BottomNavigationAction label="2004"/>
                 <BottomNavigationAction label="2008"/>
