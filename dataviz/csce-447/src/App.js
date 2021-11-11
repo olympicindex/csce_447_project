0b2ecf0330f6eb448a5a79f6abcf544e6871b768
rturovilla <rturovilla@tamu.edu>
2021-11-10 18:33:19 -0600

    added and deleted some files for navbar components

--- dataviz/csce-447/src/App.js
+++ dataviz/csce-447/src/App.js
@@ -1,10 +1,12 @@
-import react from "react";
+import React from "react";
 import './App.css';
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>hello bitch </h1>
+      <Navbar/>
     </div>
   );
 }
